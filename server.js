6ee370de57a8adf53150e3acb86a4f55ec2c8c51
joanMRomero <joanmr87@gmail.com>
2021-06-03 12:13:52 -0300

    Agrego middleware express.json

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const PORT = 3000;
 const app = express();
 
 app.get('/', (req, res) => {
-    console.log(`Probando si funciona y mirando que viene en req`, req);
+    console.log(`Probando si funciona`);
     res.send(`Esto sale del parametro res de app.get`)
 });
+
+app.use(express.json());
 
 app.listen(PORT, () => {
     console.log(`Escuchando en puerto ${PORT}`)
