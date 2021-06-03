bbcff7986731b4cd644bdf8d1f94f2e19b25cff9
joanMRomero <joanmr87@gmail.com>
2021-06-03 11:27:08 -0300

    Modificacion de script start

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
-//Esto me lo agrego solo 
-const { response } = require('express');
+
 const express = require('express');
-//Esto me lo agrego solo 
-const { clearScreenDown } = require('readline');
 const PORT = 3000;
 const app = express();
 
 app.get('/', (req, res) => {
-    console.log(`Probando si funciona y mirando que viene en req:${req}`);
+    console.log(`Probando si funciona y mirando que viene en req`, req);
     res.send(`Esto sale del parametro res de app.get`)
 });
 
