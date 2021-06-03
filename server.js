555ada063b1db5664195a9cf5f13382d6733e33a
joanMRomero <joanmr87@gmail.com>
2021-06-03 12:29:46 -0300

    Se crea directorio public con index.html y conecta al server con middleware static

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ const express = require('express');
 const PORT = 3000;
 const app = express();
 
-app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
     console.log(`Probando si funciona`);
     res.send(`Esto sale del parametro res de app.get`)
 });
+*/
 
 app.use(express.json());
+
+app.use(express.static('./public'));
 
 app.listen(PORT, () => {
     console.log(`Escuchando en puerto ${PORT}`)
