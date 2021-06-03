1d661d51b05183e6ecfad46c9e7ba52ff8140796
joanMRomero <joanmr87@gmail.com>
2021-06-03 16:35:56 -0300

    Creo archivo database

---
+++ src/database.js
@@ -0,0 +1,11 @@
+/*BASE DE DATOS DE PRUEBA PARA DEJAR HECHOS LOS ROUTING */
+
+const DB = [{
+            ID: 1,
+            Tarea: 'Hacer el Trabajo Practico de PW2',
+            Estado: 'Pendiente'
+            }];
+
+module.exports = {
+    DB
+};
