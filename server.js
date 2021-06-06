9e7f21525de30c20970b1e42f2f40e11707e9888
joanMRomero <joanmr87@gmail.com>
2021-06-06 10:51:16 -0300

    Creo archivo apiHandlers.js

--- server.js
+++ server.js
@@ -5,27 +5,8 @@ const database = require('./src/database')
 const app = express();
 app.use(express.json());
 
-const routingTareas = express.Router();
+const routingTareas = require('./apiHandlers')
 
-routingTareas.get('/', (req, res) => {
-    console.log(`Metodo GET funcionando`);
-    res.json(database.DB)
-});
-
-routingTareas.post('/', (req, res) => {
-    const tarea = req.body.tarea;   
-    const estado = req.body.estado
-
-    const tareaDeUsuario = {
-        tarea: tarea,
-        estado: estado
-    };
-    
-    database.DB.push(tareaDeUsuario);
-    res.json(tareaDeUsuario)
-    console.log('La tarea fue agregada');
-    
-});
 
 app.use('/tareas', routingTareas);
 
