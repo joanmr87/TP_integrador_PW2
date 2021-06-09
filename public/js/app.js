4315765f6ce94c2107d8f87afdfb7db3c2107964
Martin Gonzalez <gonzalez.martin.mag@gmail.com>
2021-06-09 14:58:09 -0300

    Template list item

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,55 @@
 // Variables
-const formTareas = document.querySelector('#form-tareas');
+const formTask = document.querySelector('#form-tareas');
+const templateTask = document.querySelector('#task-item').content;
+const taskList = document.querySelector('#task-list');
 
 // Listeners
-formTareas.addEventListener('submit', submitFormTarea);
+formTask.addEventListener('submit', submitFormTask);
+document.addEventListener('DOMContentLoaded', () => {
+    listTasks();
+});
 
-function submitFormTarea(e) {
+function addTask(name, description, id) {
+    const item = templateTask.cloneNode(true);
+
+    item.querySelector('.task-name').textContent = name;
+    item.querySelector('.task-description').textContent = description;
+    item.querySelector('li').dataset.id = id;
+
+    taskList.appendChild(item);
+}
+
+/**
+ * 
+ * @param {'GET' | 'POST' | 'PUT' | 'DELETE'} method 
+ * @param {'/' | '/:id'} url 
+ * @param {*} body 
+ * @returns 
+ */
+async function callApi(method, url, body = undefined) {
+    const resp = await fetch(`/tareas${url}`, {
+        method: method
+    });
+    const data = await resp.json();
+    return data;
+}
+
+async function listTasks() {
+    const tasks = await callApi('GET', '/');
+    console.log(tasks);
+
+}
+
+function submitFormTask(e) {
     e.preventDefault();
 
-    const nombre = this.querySelector('#nombre-tarea').value;
-    const descripcion = this.querySelector('#descripcion').value;
+    const name = this.querySelector('#nombre-tarea').value;
+    const description = this.querySelector('#descripcion').value;
 
-    if (nombre && descripcion) {
-        const tarea = { nombre, descripcion };
-        console.log(tarea);
+    if (name && description) {
+        const task = { name, description };
+        addTask(name, description, 2)
+        console.log(task);
     } else {
         console.log("Faltan datos");
     }
