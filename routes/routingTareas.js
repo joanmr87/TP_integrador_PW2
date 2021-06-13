8044ee8430f478ad3450b89a8cbd99ac1e079a09
Martin Gonzalez <gonzalez.martin.mag@gmail.com>
2021-06-13 14:25:11 -0300

    a

---
+++ routes/routingTareas.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const database = require('../src/database')
+const { body, validationResult } = require('express-validator');
+
+const routingTareas = express.Router();
+
+routingTareas.get('/', (req, res) => {
+    console.log(`Metodo GET funcionando`);
+    res.json(database.DB)
+});
+
+routingTareas.post('/',
+    body('tarea').notEmpty().withMessage('Campo obligatorio'),
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+
+
+        const tareaDeUsuario = {
+            Id: req.body.Id,
+            tarea: req.body.tarea,
+            descripcion: req.body.descripcion,
+        };
+
+        database.DB.push(tareaDeUsuario);
+        res.json(tareaDeUsuario)
+        console.log('La tarea fue agregada');
+
+    });
+
+module.exports = routingTareas;
--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-
 const express = require('express');
-const {PORT} = require('./src/config');
+const { PORT } = require('./src/config');
 const database = require('./src/database')
+const routingTareas = require('./routes/routingTareas')
+
 const app = express();
 app.use(express.json());
 
-const routingTareas = require('./src/routingTareas')
 
 
-app.use('/tareas', routingTareas);
+app.use('/api/tareas', routingTareas);
 
 app.use(express.static('./public'));
 
 (async () => {
-    await database.initDB();
-    app.listen(APP_PORT, () => {
-      console.info(`Oyendo en puerto ${APP_PORT}`);
-    });
-  })();
+  await database.initDB();
+  app.listen(APP_PORT, () => {
+    console.info(`Oyendo en puerto ${APP_PORT}`);
+  });
+})();
